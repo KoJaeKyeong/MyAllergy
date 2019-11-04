fdfcc84be178229b185ab9e6a5fefca7d0dfb323
고재경 <folw159@gmail.com>
2019-11-04 22:08:43 +0900

    JK

--- app/src/main/java/com/example/myallergy/Retrofit2/WebEndPoint.java
+++ app/src/main/java/com/example/myallergy/Retrofit2/WebEndPoint.java
@@ -5,7 +5,6 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
@@ -31,7 +30,6 @@ public interface WebEndPoint {
     @GET("MyAllergy/community")
     Call<List<PostVO>> searchCommunity();
 
-    @Multipart
     @POST("MyAllergy/community")
     Call<PostVO> sendCommunity(@Body PostVO post);
 
